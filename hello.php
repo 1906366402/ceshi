6c7f629428d13caed0970231bfdfb7c614f73b04
1906366402 <1906366402@qq.com>
2016-08-01 19:11:41 +0800

    1

--- hello.php
+++ hello.php
@@ -1,36 +1,6 @@
 <?php
 
-hello,'男男';
-
-echo "dd";
-
-echo "已经改变";
-
-echo "又一次改变";
-
-echo "再一次改变";
-
-echo "d";
-
-echo "啊";
-
-echo "这么回事";
-
-echo "再试一次";
-
-echo "客户端的一次";
-
-echo "命令";
-
-echo "jintianzuihouyici";
-
 echo "你不是真正的快乐";
-
-echo "哈哈哈哈";  
-
-echo "你不是真正 点快了"; 
- 
-echo "我马上";
 
 
 ?>
